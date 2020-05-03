8a9937fdd758ab0c5374500a7a3204c257fcbaf7
dmrval <hal9van1@gmail.ru>
2020-05-03 23:12:16 +0400

    second commit conflict test

--- gold-status-producer/src/main/java/ru/dmrval/goldstatusproducer/repository/BankAccountInfoRepository.java
+++ gold-status-producer/src/main/java/ru/dmrval/goldstatusproducer/repository/BankAccountInfoRepository.java
@@ -2,9 +2,11 @@ package ru.dmrval.goldstatusproducer.repository;
 
 import reactor.core.publisher.Flux;
 import ru.dmrval.goldstatusproducer.model.BankAccountInfo;
-import ru.dmrval.goldstatusproducer.requestbankaccountinfos.BankAccountInfoRequest;
 
 public interface BankAccountInfoRepository {
-
   Flux<BankAccountInfo> saveAll();
+
+  default void test() {
+    System.out.println();
+  }
 }
